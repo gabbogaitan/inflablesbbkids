dd309dca51b0bd071981642ecab064e9d6ea9a57
gabbogaitan <103861426+gabbogaitan@users.noreply.github.com>
2024-11-16 01:27:39 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,7 +1,7 @@
 function mostrarAlerta(){
   alert("su mensaje fue enviado");
 }
-function sendmessages() {
+/*function sendmessages() {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
@@ -12,10 +12,12 @@ function sendmessages() {
     var mailtoLink = 'mailto:gaitan.gabriel@hotmail.com?subject=' + encodeURIComponent('Mensaje de ' + name) + '&body=' + encodeURIComponent('Nombre: ' + name + '\nCorreo: ' + email + '\nMensaje: ' + message);
     window.open(mailtoLink, '_blank');
     mostrarAlerta();
-  }
+  }*/
   
 function sendwhatsapp(){
-
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
    // Enviar mensaje por WhatsApp
     var whatsappURL = 'https://wa.me/+5491123909541?text=' + encodeURIComponent('Nombre: ' + name + '\nCorreo: ' + email + '\nMensaje: ' + message);
     window.open(whatsappURL, '_blank');
