a540c405e525faba9004af02b3905a18da8e5766
gabbo <gabbogaitan@gmail.com>
2024-12-20 13:00:19 -0300

    actualizando repo

--- funciones.js
+++ funciones.js
@@ -1,25 +1,28 @@
 function mostrarAlerta(){
   alert("su mensaje fue enviado");
 }
-/*function sendmessages() {
+function sendmessages() {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
   
     
   
-    // Enviar mensaje por Email
+     //Enviar mensaje por Email
     var mailtoLink = 'mailto:gaitan.gabriel@hotmail.com?subject=' + encodeURIComponent('Mensaje de ' + name) + '&body=' + encodeURIComponent('Nombre: ' + name + '\nCorreo: ' + email + '\nMensaje: ' + message);
     window.open(mailtoLink, '_blank');
     mostrarAlerta();
-  }*/
+  }
   
-function sendwhatsapp(){
+/*function sendwhatsapp(){
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
+
    // Enviar mensaje por WhatsApp
     var whatsappURL = 'https://wa.me/+5491123909541?text=' + encodeURIComponent('Nombre: ' + name + '\nCorreo: ' + email + '\nMensaje: ' + message);
     window.open(whatsappURL, '_blank');
     mostrarAlerta();
-  }
+  }*/
+
+    
